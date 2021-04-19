ee4ba974b89defe3a792bfbfd075abdb0e2ecb9c
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-04-19 18:02:09 +0530

    Add files via upload

---
+++ virtual pet 2/js/Food.js
@@ -0,0 +1,27 @@
+class Food {
+    constructor(){
+    this.foodStock=0;
+    this.lastFed;
+    this.image=loadImage('Images/Milk.png');
+    }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
+    //create display function
+    
